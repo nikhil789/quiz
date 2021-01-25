203e9482625f82d2bff2d4ccbd556a397e1d55ec
akhil <akhil.anurag1999@gmail.com>
2021-01-25 20:36:41 +0530

    new test added

--- all_test.js
+++ all_test.js
@@ -77,6 +77,10 @@ var Test_data =
 
 				"title" : "Ankit Agarwal November 2",
 				"adress": "./data/ankit_ag_nov_mcq_2.js"	
-			}
+            },
+            {
+                "title" : "Ankit Janurary Quiz 1",
+                "adress": "./data/Ankit_Jan_1.js"
+            }
 		]
 }
---
+++ data/Ankit_Jan_1.js
@@ -0,0 +1,491 @@
+var DATA = {
+    "title": "Ankit January Quiz - 1",
+    "duration": "25",
+    "questions": [
+        {
+            "type": "dtq",
+            "stmt": "In India which of the following has highest share in the disbursement of credit to agriculture and allied activities?",
+            "options": [
+                "Commercial banks",
+                "Cooperative banks",
+                "Regional rural banks",
+                "Micro finance institutions "
+            ],
+            "correct": "Commercial banks"
+        },
+        {
+            "type": "dtq",
+            "stmt": "Which of the following are called as Bretton Woods twin sisters ?",
+            "options": [
+                "IMF & WTO",
+                "WTO & UNCTAD",
+                "IMF & IBRD",
+                "WTO & IBRD"
+            ],
+            "correct": "IMF &amp; IBRD"
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Consider the following statements regarding DRDO :",
+            "stmts": [
+                "It was formed after 1971 war with Pakistan.",
+                "There are more than 50 laboratories across India."
+            ],
+            "foot_stmt": "select the correct statements:",
+            "options": [
+                "1 only ",
+                "2 only",
+                "Both 1 and 2",
+                "None of these"
+            ],
+            "correct": "2 only"
+        },
+        {
+            "type": "dtq",
+            "stmt": "Which of the following is not among the members of European Communities?",
+            "options": [
+                "Italy ",
+                "Belgium ",
+                "West Germany",
+                "Spain "
+            ],
+            "correct": "Spain "
+        },
+        {
+            "type": "dtq",
+            "stmt": "Which country became the first to authorise the AstraZeneca vaccine?",
+            "options": [
+                "USA",
+                "UK",
+                "France",
+                "Japan"
+            ],
+            "correct": "UK"
+        },
+        {
+            "type": "dtq",
+            "stmt": "Which country has highest number of its citizens designated under the list of UN terrorist?",
+            "options": [
+                "Pakistan ",
+                "Afghanistan ",
+                "Iraq",
+                "Iran"
+            ],
+            "correct": "Iraq"
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Consider the following statements regarding Central Drugs Standard Control Organisation :",
+            "stmts": [
+                "Its headquarter is in Dehradun.",
+                "It was established under Drugs and Cosmetics Act 1940."
+            ],
+            "foot_stmt": "select the correct statement(s):",
+            "options": [
+                "1 only ",
+                "2 only",
+                "Both 1 and 2",
+                "None of these"
+            ],
+            "correct": "2 only"
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Consider the following statements regarding Securities Appellate Tribunal :",
+            "stmts": [
+                "It is a statutory body established under the provisions of SEBI Act, 1992.",
+                "It only hears appeal against orders passed by SEBI."
+            ],
+            "foot_stmt": "select the correct statement(s):",
+            "options": [
+                "1 only ",
+                "2 only",
+                "Both 1 and 2",
+                "None of these"
+            ],
+            "correct": "1 only "
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Which of the following comes under the Concurrent List of 7th Schedule ?",
+            "stmts": [
+                "Forest",
+                "Trade Unions ",
+                "Marriage ",
+                "Communication ",
+                "Police "
+            ],
+            "foot_stmt": "",
+            "options": [
+                "1, 2, 3",
+                "2, 4, 5",
+                "2, 3, 5",
+                "1, 2, 3, 4"
+            ],
+            "correct": "1, 2, 3"
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Arrange the following in order of their formation regarding the initiatives of govt. to tackle cyber security.",
+            "stmts": [
+                "National Cyber Security Strategy ",
+                "Information and Technology Act",
+                "Cyber Surakshit Bharat Initiative "
+            ],
+            "foot_stmt": "",
+            "options": [
+                "1 - 2 - 3",
+                "2 - 3 - 1",
+                "1 - 3 - 2",
+                "2 - 1 - 3"
+            ],
+            "correct": "2 - 3 - 1"
+        },
+        {
+            "type": "dtq",
+            "stmt": "Which of the following statements regarding abortion in India is correct?",
+            "options": [
+                "Legal only up to 20 weeks of pregnancy ",
+                "Legal only up to 32 weeks of pregnancy ",
+                "It is completely illegal ",
+                "Abortion is completely legal"
+            ],
+            "correct": "Legal only up to 20 weeks of pregnancy "
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Which of the following are correct regarding the immunisation program of India ?",
+            "stmts": [
+                "Universal Immunization Program (UIP) was launched in 1985.",
+                "It is largely responsible for reducing the under 5 mortality rate.",
+                "Meningitis is part of UIP."
+            ],
+            "foot_stmt": "",
+            "options": [
+                "1 and 2",
+                "2 only ",
+                "1 and 3",
+                "All of these"
+            ],
+            "correct": "All of these"
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Consider the following statements regarding Forex Reserve:",
+            "stmts": [
+                "Since 2000, India’s forex reserves never fallen below $100 billion.",
+                "China has the highest forex reserve in the world."
+            ],
+            "foot_stmt": "select the correct statement(s) :",
+            "options": [
+                "1 only ",
+                "2 only ",
+                "Both 1 and 2",
+                "None of these"
+            ],
+            "correct": "2 only "
+        },
+        {
+            "type": "dtq",
+            "stmt": "The first textile mill was started in Bombay in 1853. But the first jute mill was stated at which place in India?",
+            "options": [
+                "Kolkata ",
+                "Bardaman ",
+                "Rishra",
+                "Jamshedpur"
+            ],
+            "correct": "Rishra"
+        },
+        {
+            "type": "dtq",
+            "stmt": "Which of the following period is considered as the Delhi Renaissance?",
+            "options": [
+                "1800 - 1830",
+                "1830 - 1857",
+                "1857 - 1880",
+                "1880 - 1900"
+            ],
+            "correct": "1830 - 1857"
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Consider the following statements regarding the President of India :",
+            "stmts": [
+                "The value of the vote of each MLA varies from state to state.",
+                "Value of the vote of MPs of the Lok Sabha is more than the value of the vote of MPs of the Rajya Sabha."
+            ],
+            "foot_stmt": "select the correct statement(s) :",
+            "options": [
+                "1 only ",
+                "2 only ",
+                "Both 1 and 2",
+                "None of these"
+            ],
+            "correct": "Both 1 and 2"
+        },
+        {
+            "type": "dtq",
+            "stmt": "For which of the following diseases, India recently inaugurated first indigenously developed vaccine?",
+            "options": [
+                "Pneumonia",
+                "Hepatitis B",
+                "Measles",
+                "None of these"
+            ],
+            "correct": "Pneumonia"
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Which of the following Dams are correctly matched with the states ?",
+            "stmts": [
+                "Bhavani Sagar Dam - Karnataka",
+                "Tungabhadra Dam - Tamil Nadu",
+                "Rihand Dam - Uttar Pradesh",
+                "Koyna Dam - Maharashtra "
+            ],
+            "foot_stmt": "",
+            "options": [
+                "1 and 2",
+                "2 and 3",
+                "3 and 4",
+                "2, 3 and 4"
+            ],
+            "correct": "3 and 4"
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Which of the following statements are correct regarding Dedicated Freight Corridor (DFC) ?",
+            "stmts": [
+                "While eastern DFC is developed by the World Bank assistance, western DFC is developed by Japanese assistance.",
+                "Western DFC is longer than eastern DFC."
+            ],
+            "foot_stmt": "",
+            "options": [
+                "1 only ",
+                "2 only ",
+                "Both 1 and 2",
+                "None of these"
+            ],
+            "correct": "1 only "
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Which of the following statements are correct regarding Electric Vehicles (EV) initiative in India ?",
+            "stmts": [
+                "FAME India was launched in 2015 with the objective to support hybrid/EV market.",
+                "Govt. has set a target of EV making up 50% of new sales of cars and two-wheelers by 2030."
+            ],
+            "foot_stmt": "",
+            "options": [
+                "1 only ",
+                "2 only ",
+                "Both 1 and 2",
+                "None of these"
+            ],
+            "correct": "1 only "
+        },
+        {
+            "type": "dtq",
+            "stmt": "Which of the following effects is expected to have on appreciation of rupee ?",
+            "options": [
+                "Increase in exports from India",
+                "Increase in imports to India ",
+                "Higher inflation in India ",
+                "Increase in output in India "
+            ],
+            "correct": "Increase in imports to India "
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Which of the following statements are correct regarding Gulf Cooperation Council (GCC) and India ?",
+            "stmts": [
+                "UAE and Saudi Arabia are India’s third and fourth largest trading partners.",
+                "India and Oman hold annual bilateral army exercise called Al-Najah."
+            ],
+            "foot_stmt": "",
+            "options": [
+                "1 only ",
+                "2 only ",
+                "Both 1 and 2",
+                "None of these"
+            ],
+            "correct": "Both 1 and 2"
+        },
+        {
+            "type": "dtq",
+            "stmt": "Which of the following country ranks lowest in neonatal mortality rate ( deaths per 1000 live births ) ?",
+            "options": [
+                "India",
+                "Sri Lanka",
+                "Bangladesh",
+                "Nepal"
+            ],
+            "correct": "India"
+        },
+        {
+            "type": "dtq",
+            "stmt": "Which of the following disease is caused by a virus ?",
+            "options": [
+                "Measles",
+                "Leprosy",
+                "Pneumonia ",
+                "Typhoid"
+            ],
+            "correct": "Measles"
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Which of the following steps can greatly reduce navigation time and distance between India and East Asia ?",
+            "stmts": [
+                "Deepening of the Malacca strait between Malaysia and Indonesia.",
+                "Opening a new canal across the Kra isthmus between the gulf of Siam and Andaman sea."
+            ],
+            "foot_stmt": "",
+            "options": [
+                "1 only ",
+                "2 only ",
+                "Both 1 and 2",
+                "None of these"
+            ],
+            "correct": "2 only "
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Which of the following statements are correct regarding Taiwan ?",
+            "stmts": [
+                "No country around the world recognises Taiwan as a separate nation.",
+                "There are no bilateral investment agreement between India and Taiwan."
+            ],
+            "foot_stmt": "",
+            "options": [
+                "1 only ",
+                "2 only ",
+                "Both 1 and 2",
+                "None of these"
+            ],
+            "correct": "None of these"
+        },
+        {
+            "type": "dtq",
+            "stmt": "If a candidate is declared elected to the Lok Sabha but loses his deposit, it means that : ",
+            "options": [
+                "The polling was very poor",
+                "The election was for a multi member constituency",
+                "The elected candidate’s victory over his nearest rival was very marginal ",
+                "A very large number of candidates contested the election "
+            ],
+            "correct": "A very large number of candidates contested the election "
+        },
+        {
+            "type": "dtq",
+            "stmt": "The total installed capacity of electricity production in India is around 375 GW. The highest contribution comes from : ",
+            "options": [
+                "Central sector ",
+                "State sector ",
+                "Private sector ",
+                "All of them contribute almost equally "
+            ],
+            "correct": "Private sector "
+        },
+        {
+            "type": "dtq",
+            "stmt": "Transfer of property is mentioned under which list of 7th Schedule ?",
+            "options": [
+                "Union list",
+                "State list",
+                "Concurrent list",
+                "not mentioned in any list"
+            ],
+            "correct": "Concurrent list"
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Arrange the different stages that stars undergo to form a black hole ?",
+            "stmts": [
+                "Stella nebula",
+                "Massive star",
+                "Red supergiant ",
+                "Supernova ",
+                "Black hole"
+            ],
+            "foot_stmt": "",
+            "options": [
+                "1 - 2 - 3 - 4 - 5",
+                "1 - 2 - 4 - 3 - 5",
+                "2 - 1 - 3 - 4 - 5",
+                "2 - 1 - 4 - 3 - 5"
+            ],
+            "correct": "1 - 2 - 3 - 4 - 5"
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Consider the following statements regarding Call Money market :",
+            "stmts": [
+                "This is basically an inter bank market where funds are borrowed and lent for one day.",
+                "Collateral security is not required to borrow from this market."
+            ],
+            "foot_stmt": "select the correct statement(s) : ",
+            "options": [
+                "1 only",
+                "2 only",
+                "Both 1 and 2",
+                "None of these"
+            ],
+            "correct": "1 only"
+        },
+        {
+            "type": "dtq",
+            "stmt": "Most militaries in the world maintain court martial system to try cases related to military discipline. Which of the following doesn’t have court martial at the time of peace?",
+            "options": [
+                "Germany",
+                "Singapore ",
+                "United Kingdom ",
+                "Indonesia"
+            ],
+            "correct": "Germany"
+        },
+        {
+            "type": "dtq",
+            "stmt": "Which country has the most electric vehicle per population?",
+            "options": [
+                "Norway",
+                "UK",
+                "China",
+                "USA"
+            ],
+            "correct": "Norway"
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Consider the following statements regarding Environmental Performance Index :",
+            "stmts": [
+                "It is released every year.",
+                "It is prepared by Yale University and Columbia University in collaboration with the World Bank."
+            ],
+            "foot_stmt": "select the correct statement(s) : ",
+            "options": [
+                "1 only",
+                "2 only",
+                "Both 1 and 2",
+                "None of these"
+            ],
+            "correct": "None of these"
+        },
+        {
+            "type": "msq",
+            "head_stmt": "Consider the following statements regarding Lithium :",
+            "stmts": [
+                "Lithium was discovered in 1817 by Norwegian chemist Johann Arfvedson.",
+                "Lithium is found in grains and vegetables and even in drinking water."
+            ],
+            "foot_stmt": "select the correct statement(s) : ",
+            "options": [
+                "1 only",
+                "2 only",
+                "Both 1 and 2",
+                "None of these"
+            ],
+            "correct": "2 only"
+        }
+    ]
+};
