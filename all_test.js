23a149e9a4dea63374e14723a3989d4e1ceeb241
akhil <akhil.anurag1999@gmail.com>
2021-01-27 10:42:34 +0530

    old tests removed

--- all_test.js
+++ all_test.js
@@ -3,46 +3,7 @@ var Test_data =
 
 		"Tests" : 
 		[ 
-			{
-				"title" : "January",
-				"adress": "./data/JAN.js"
-			},
-
-			{
-				"title" : "February",
-				"adress": "./data/FEB.js"
-			},
 			
-			{
-				"title" : "April",
-				"adress": "./data/april.js"
-			},
-
-			{
-
-				"title" : "August",
-				"adress": "./data/AUG.js"	
-			},
-			{
-
-				"title" : "September",
-				"adress": "./data/SEPT.js"	
-			},
-			{
-
-				"title" : "October",
-				"adress": "./data/OCT.js"	
-			},
-			{
-
-				"title" : "November",
-				"adress": "./data/NOV.js"	
-			},
-			{
-
-				"title" : "Bihar Current Affairs",
-				"adress": "./data/bihar_test_1.js"	
-			},
 			{
 
 				"title" : "Fundamental duties",
@@ -53,21 +14,7 @@ var Test_data =
 				"title" : "January defence",
 				"adress": "./data/jan_20_defence_news.js"	
 			},
-			{
-
-				"title" : "July",
-				"adress": "./data/JULY.js"	
-			},
-			{
-
-				"title" : "June",
-				"adress": "./data/june.js"	
-			},
-			{
-
-				"title" : "May",
-				"adress": "./data/may.js"	
-			},
+			
 			{
 
 				"title" : "Ankit Agarwal November 1",
