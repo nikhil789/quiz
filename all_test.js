f7f181f3a095e7056a7b001c78cdd110bc2f01aa
nikhil789 <nikhil789@users.noreply.github.com>
2022-08-21 21:37:41 +0530

    Update all_test.js

--- all_test.js
+++ all_test.js
@@ -5,120 +5,24 @@ var Test_data =
 		[ 
             
 			{
-                "title" : "June 2021",
-                "adress": "./data/june_2021_mcqs.js"
-            },
+            		    	 "title" : "June 2021",
+               			 "adress": "./data/june_2021_mcqs.js"
+            		},
 			{
-                "title" : "May 2021",
-                "adress": "./data/may_2021_mcqs.js"
-            },
-            {
-                "title" : "January 2021",
-                "adress": "./data/january_2021_mcqs.js"
-            },
+                		"title" : "May 2021",
+               			"adress": "./data/may_2021_mcqs.js"
+            		},
 			{
 
 				"title" : "Fundamental duties",
 				"adress": "./data/fundamental_duties.js"	
 			},
+		
 			{
 
-				"title" : "January defence",
-				"adress": "./data/jan_20_defence_news.js"	
-			},
-			
-			{
-
-				"title" : "Ankit Agarwal November 1",
-				"adress": "./data/ankit_ag_nov_mcq.js"	
-			},
-			{
-
-				"title" : "Ankit Agarwal November 2",
-				"adress": "./data/ankit_ag_nov_mcq_2.js"	
-            },
-            {
-                "title" : "Ankit Janurary Quiz 1",
-                "adress": "./data/Ankit_Jan_1.js"
-            },
-            {
-                "title" : "Ankit Janurary Quiz 2",
-                "adress": "./data/ankit_january_quiz_2.js"
-            },
-            {
-                "title" : "Polity Difficult Questions",
-                "adress": "./data/polity_difficult_questions.js"
-            },
-            {
-                "title" : "Current Affairs Jan 1 (2021)",
-                "adress": "./data/jan_current_affairs_1.js"
-            },
-			{
-                "title" : "Current Affairs Jan 2 (2021)",
-                "adress": "./data/jan_current_affairs_2.js"
-            },
-			{
-                "title" : "Current Affairs Feb 1 (2021)",
-                "adress": "./data/feb_current_affairs_1.js"
-            },
-			{
-                "title" : "Current Affairs Feb 2 (2021)",
-                "adress": "./data/feb_current_affairs_2.js"
-            },
-			{
-                "title" : "Current Affairs March 1 (2021)",
-                "adress": "./data/march_current_affairs_1.js"
-            },
-			{
-                "title" : "Current Affairs March 2 (2021)",
-                "adress": "./data/march_current_affairs_2.js"
-            },
-			{
-                "title" : "Current Affairs April 1 (2021)",
-                "adress": "./data/april_current_affairs_1.js"
-            },
-			{
-                "title" : "Current Affairs Dec 1",
-                "adress": "./data/dec_cur_affairs_1.js"
-            },
-			{
-                "title" : "Current Affairs Dec 2",
-                "adress": "./data/dec_cur_affairs_2.js"
-            },
-			{
-                "title" : "Current Affairs Nov 1",
-                "adress": "./data/nov_cur_affairs_1.js"
-            },
-			{
-                "title" : "Current Affairs Nov 2",
-                "adress": "./data/nov_cur_affairs_2.js"
-            },
-			{
-                "title" : "Current Affairs Oct 1",
-                "adress": "./data/oct_cur_affairs_1.js"
-            },
-			
-			{
-                "title" : "Current Affairs Oct 2",
-                "adress": "./data/oct_cur_affairs_2.js"
-            },
-			
-			{
-                "title" : "Current Affairs Sep 1",
-                "adress": "./data/sept_cur_affairs_1.js"
-            },
-			{
-                "title" : "Current Affairs Sep 2",
-                "adress": "./data/sept_cur_affairs_2.js"
-            },
-			{
-                "title" : "Current Affairs Aug 1",
-                "adress": "./data/aug_cur_affairs_1.js"
-            },
-			{
-                "title" : "Current Affairs Aug 2",
-                "adress": "./data/aug_cur_affairs_2.js"
-            }
+				"title" : "67th BPSC",
+				"adress": "./data/67thbpsc(cancel).js"	
+			}
 			
 			
 			
